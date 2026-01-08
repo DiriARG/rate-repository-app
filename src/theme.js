e4d7f0e3f6907583547b3112f3f7f46b8971cad7
DiriARG <matidirisio@gmail.com>
2026-01-08 18:07:24 -0300

    Ejercicio 10.10 resuelto.

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,24 @@
 // Código copiado de la teoría.
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
     textSecondary: "#586069",
     primary: "#0366d6",
-    colorDeFondo: "#e1e4e8"
+    colorDeFondo: "#e1e4e8",
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    // "Platform.select" elige automáticamente según el SO del usuario. 
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
