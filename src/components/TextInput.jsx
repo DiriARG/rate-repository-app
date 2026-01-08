069759ca4a59070d9e346ac44253f69ae26fbec3
DiriARG <matidirisio@gmail.com>
2026-01-08 10:35:40 -0300

    FormikTextInput.jsx y TextInput.jsx.

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,13 @@
+// Código copiado de la teoría.
+import React from 'react';
+import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const TextInput = ({ style, error, ...props }) => {
+  const textInputStyle = [style];
+
+  return <NativeTextInput style={textInputStyle} {...props} />;
+};
+
+export default TextInput;
