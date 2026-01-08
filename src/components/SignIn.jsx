7673812fb4a9863ed4368bdf5597366214ebe2da
DiriARG <matidirisio@gmail.com>
2026-01-08 11:33:45 -0300

    Ejercicio 10.8 resuelto.

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,67 @@
-import Text from './Text';
+import { View, StyleSheet, Pressable } from "react-native";
+import { Formik } from "formik";
+
+import FormikTextInput from "./FormikTextInput";
+import Text from "./Text";
+import theme from "../theme";
+
+const estilos = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: "white",
+  },
+  input: {
+    marginBottom: 10,
+  },
+  boton: {
+    backgroundColor: theme.colors.primary,
+    paddingVertical: 15,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  textoDelBoton: {
+    color: "white",
+  },
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    /* Formik es el contenedor principal que maneja el estado del formulario.
+    - initialValues (Obligatorio): Define los nombres de los campos y sus valores iniciales.
+    - onSubmit (Obligatorio): Función que se dispara cuando el formulario es válido y se envía.
+    */
+    <Formik initialValues={{ usuario: "", contraseña: "" }} onSubmit={onSubmit}>
+      {/* Se utiliza el patrón "render prop" para extraer "handleSubmit" de Formik. 
+      Esta función actúa como mediadora: valida el formulario (al hacer click en el "Pressable") y, si todo es correcto, ejecuta automáticamente la función "onSubmit" definida anteriormente. */}
+      {({ handleSubmit }) => (
+        <View style={estilos.container}>
+          <FormikTextInput
+            name="usuario"
+            placeholder="Username"
+            style={estilos.input}
+          />
+
+          <FormikTextInput
+            name="contraseña"
+            placeholder="Password"
+            // Propiedad de React Native para ocultar caracteres.
+            secureTextEntry
+            style={estilos.input}
+          />
+
+          <Pressable onPress={handleSubmit} style={estilos.boton}>
+            <Text fontWeight="bold" style={estilos.textoDelBoton}>
+              Sign in
+            </Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
+  );
 };
 
 export default SignIn;
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,11 +1,29 @@
 // Código copiado de la teoría.
-import React from 'react';
-import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
+import React from "react";
+import { TextInput as NativeTextInput, StyleSheet } from "react-native";
+import theme from "../theme";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    // Grosor del rectángulo del input.
+    borderWidth: 1,
+    borderColor: "#d0d7de",
+    borderRadius: 4,
+    padding: 15,
+    backgroundColor: "white",
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+  },
+  error: {
+    borderColor: "#d73a4a",
+  },
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  /* "styles.input": Siempre aplica el diseño base (bordes, padding). 
+  "error && styles.error": Si la prop "error" es verdadera (true), el borde rojo sobreescribe al gris por estar después en el array. Si no hay error (false), esta capa simplemente no existe y se salta. 
+  "style": Permite aplicar estilos adicionales enviados desde otros componentes. */
+  const textInputStyle = [styles.input, error && styles.error, style];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
