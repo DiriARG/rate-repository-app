6ee6597a8e8a14a234b98017c6564fd551614a4d
DiriARG <matidirisio@gmail.com>
2026-01-09 23:49:13 -0300

    Variables de entorno.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client/react";
+import Constants from 'expo-constants';
 
 import Main from "./src/components/Main";
 import createApolloClient from "./src/utils/apolloClient";
@@ -8,9 +9,11 @@ import createApolloClient from "./src/utils/apolloClient";
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.expoConfig);
+
   return (
     <>
-      {/* Se envuelve "Main" con NativeRouter para habilitar la navegación en la app.*/}
+      {/* Se envuelve "Main" con NativeRouter para habilitar la navegación en la app. */}
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
           <Main />
