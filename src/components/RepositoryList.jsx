f0238466402fd751f4697e9cfea18f19443df029
DiriARG <matidirisio@gmail.com>
2026-01-11 14:53:53 -0300

    "Prueba de aplicaciones React Native", "Prueba de componentes" y "Manejo de dependencias en pruebas".

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,6 @@
-import { FlatList, View, StyleSheet} from 'react-native';
-import RepositoryItem from './RepositoryItem';
-import useRepositories from '../hooks/useRepositories';
+import { FlatList, View, StyleSheet } from "react-native";
+import RepositoryItem from "./RepositoryItem";
+import useRepositories from "../hooks/useRepositories";
 
 const styles = StyleSheet.create({
   separator: {
@@ -8,21 +8,18 @@ const styles = StyleSheet.create({
   },
 });
 
-const ItemSeparator = () => <View style={styles.separator} />
+const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryList = () => {
-  // repositories = data.repositories.
-  const { repositories } = useRepositories()
-
-  /* Por lo tanto si hay "data" se transforma la siguiente estructura: { repositories: { edges: [ { node: { ... } } ] } }, se convierte en un array plano: [ { ... }, { ... } ] mapeando "edges" y sustrayendo por cada "edge" un "node" que sería un repositorio. */
+export const RepositoryListContainer = ({ repositories }) => {
+  // Por lo tanto si hay "data" se transforma la siguiente estructura: { repositories: { edges: [ { node: { ... } } ] } }, se convierte en un array plano: [ { ... }, { ... } ] mapeando "edges" y sustrayendo por cada "edge" un "node" que sería un repositorio.
   const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
+    ? repositories.edges.map((edge) => edge.node)
     : [];
 
   return (
     <FlatList
       // Ahora se utilizan los datos del servidor.
-      data={repositoryNodes} 
+      data={repositoryNodes}
       // "renderItem" es una función que FlatList ejecuta por cada elemento del array "repositoryNodes"; Se extrae {item}, que es el objeto con la info del repositorio actual en cada iteración.
       renderItem={({ item }) => (
         // Se envía la info del repo a traves de la prop "repositorio".
@@ -35,7 +32,11 @@ const RepositoryList = () => {
   );
 };
 
+const RepositoryList = () => {
+  // repositories = data.repositories.
+  const { repositories } = useRepositories();
+
+  return <RepositoryListContainer repositories={repositories} />;
+};
+
 export default RepositoryList;
-
-
-
